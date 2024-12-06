721ec66e16af87380ca2b679a68f369f313af86f
fowlinbasket <foul.in.basket@gmail.com>
2024-12-06 15:20:10 -0700

    Initialized remaining files

---
+++ day13.py
@@ -0,0 +1,10 @@
+
+def part1(filename):
+    return 0
+
+def part2(filename):
+    return 0
+
+if __name__ == '__main__':
+    print(f'Solution to Part 1: {part1('day0X.txt')}')
+    print(f'Solution to Part 2: {part2('day0X.txt')}')
